78231911bf082e53e1a65b755ea4720710b56b88
mschmal2 <maximilian_dirk.schmalenbach@smail.th-koeln.de>
2023-01-16 15:16:56 +0100

    added notebook

--- augment.py
+++ augment.py
@@ -2,17 +2,14 @@ import numpy as np
 import cv2 as cv
 from keras.preprocessing.image import ImageDataGenerator
 
-traindatagen = ImageDataGenerator(featurewise_center=False, featurewise_std_normalization=False, rotation_range=20, width_shift_range=0.3, height_shift_range=0.3)
+traindatagen = ImageDataGenerator(featurewise_center=False, featurewise_std_normalization=False, 
+                                rotation_range=20, width_shift_range=0.3, height_shift_range=0.3, brightness_range=0.4)
 
-image = cv.imread('images/Lena.jpg')
+image = cv.imread('images/lena.jpg')
 images = image[np.newaxis, :, :, :]
-
-
-
 
 for i in range(6):
     # generate batch of images
     process_images = traindatagen.flow(images).next()
     process_image = process_images[0, :, :, :]
-    cv.imwrite('test_process_image.jpg', process_image)
-    cv.imwrite('lena' + str(i) + '.jpeg', process_image)  
+    cv.imwrite('images/output/lena' + str(i) + '.jpg', process_image)  
