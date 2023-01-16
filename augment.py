3d8a4f3d10f516681140aff1c57ba3994da4fb44
mschmal2 <maximilian_dirk.schmalenbach@smail.th-koeln.de>
2023-01-16 16:06:25 +0100

    final script + images

--- augment.py
+++ augment.py
@@ -5,7 +5,7 @@ import os
 
 batchSize = 10
 fileName = "Lena.jpg"
-inputDirectory = "images"
+inputDirectory = "images/input/"
 outputDirectory = "images/output/"
 
 #--- ImageDataGenerator Args---#
@@ -16,16 +16,27 @@ widthShiftRange = 0.3
 heightShiftRange = 0.3
 brightnessRange = (0.7, 1)
 
-
+def load_images_from_folder(folder):
+    images = []
+    for filename in os.listdir(folder):
+        img = cv.imread(os.path.join(folder,filename))
+        if img is not None:
+            images.append(img)
+    return images
 
 traindatagen = ImageDataGenerator(featurewise_center=featureWiseCenter, featurewise_std_normalization=featureWiseSTDNormalization,
                                   rotation_range=rotationRange, width_shift_range=widthShiftRange, height_shift_range=heightShiftRange, brightness_range=brightnessRange)
 
-image = cv.imread(inputDirectory + "/" + fileName)
-images = image[np.newaxis, :, :, :]
+allInputImages = load_images_from_folder(inputDirectory)
 
-for i in range(batchSize):
-    # generate batch of images
-    processimages = traindatagen.flow(images).next()
-    processimage = processimages[0, :, :, :]
-    cv.imwrite(outputDirectory + "/" + str(i) + fileName, processimage)
+for index, image in enumerate(allInputImages):
+    images = image[np.newaxis, :, :, :]
+
+    for i in range(batchSize):
+        # generate batch of images
+        processimages = traindatagen.flow(images).next()
+        processimage = processimages[0, :, :, :]
+        cv.imwrite(outputDirectory + str(index) + "_version" + str(i) + ".jpg", processimage)
+    
+    cv.imwrite(outputDirectory + str(index) + "_original_version" + ".jpg", image)
+
